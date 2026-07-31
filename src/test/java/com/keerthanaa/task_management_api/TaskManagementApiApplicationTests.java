9aeede2e9cf93461a4e3da520417d4cfe3e57439
Keerthanaa Chinnaswamy <keerthanaac@gmail.com>
2026-07-31 11:56:37 +0100

    Initial Spring Boot project setup

---
+++ src/test/java/com/keerthanaa/task_management_api/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keerthanaa.task_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
